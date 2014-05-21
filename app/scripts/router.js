fc24925de8f1feac6e4aac53e091cf2e42104477
Alvin Vogelzang <alvinvogelzang@gmail.com>
2014-05-21 15:04:37 +0200

    Router

--- app/scripts/router.js
+++ app/scripts/router.js
@@ -1,6 +1,10 @@
 BecomeTjunaFish.Router.map(function () {
   // Add your routes here
-  this.resource('homeLearner', { path: '/'});
+  this.resource('courses', { path: '/'});
+  this.resource('basicCourse', { path: 'basis_cursus'});
+  this.resource('frontendCourse', { path: 'frontend_cursus'});
+  this.resource('backendCourse', { path: 'backend_cursus'});
+
 
 });
 
