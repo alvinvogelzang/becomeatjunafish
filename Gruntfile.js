4a0373f3bd8ad8160fad0ba70366ef4e49d1a8ed
Alvin Vogelzang <alvinvogelzang@gmail.com>
2014-05-31 18:53:29 +0200

    Grunt removed imagemin

--- Gruntfile.js
+++ Gruntfile.js
@@ -138,9 +138,7 @@ module.exports = function (grunt) {
                 files: {
                     src: [
                         '<%= yeoman.dist %>/scripts/{,*/}*.js',
-                        '<%= yeoman.dist %>/styles/{,*/}*.css',
-                        '<%= yeoman.dist %>/images/{,*/}*.{png,jpg,jpeg,gif,webp}',
-                        '<%= yeoman.dist %>/styles/fonts/*'
+                        '<%= yeoman.dist %>/styles/{,*/}*.css'
                     ]
                 }
             }
