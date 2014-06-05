1a05dd5497191cce22bbb04d80a6fd20b8ffe135
Alvin Vogelzang <alvinvogelzang@gmail.com>
2014-06-05 10:22:28 +0200

    Topic model added content

--- app/scripts/models/topic.js
+++ app/scripts/models/topic.js
@@ -16,18 +16,24 @@ BecomeTjunaFish.Topic.FIXTURES = [
     lesson: 1,
     intro: "Dit is de inleiding over de topic Head Element. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse.",
     link_name: 'Codecademy.com',
-    external_link: 'http://www.codecademy.com'
+    external_link: 'http://www.codecademy.com/courses/html-one-o-one/1/2'
   },
   {
     id: 2,
     title: 'Div tag',
-    isCompleted: false,
-    lesson: 1
+    isCompleted: true,
+    lesson: 1,
+    intro: "Dit is de inleiding over de topic Div tag. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse.",
+    link_name: 'Developer.mozilla.org',
+    external_link: 'https://developer.mozilla.org/en-US/docs/Web/HTML/Element/div'
   },
   {
     id: 3,
     title: 'Lists',
     isCompleted: false,
-    lesson: 1
+    lesson: 1,
+    intro: "Dit is de inleiding over de topic Lists. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse.",
+    link_name: 'Developer.mozilla.org',
+    external_link: 'https://developer.mozilla.org/en-US/docs/Web/HTML/Element/ul'
   }
 ];
