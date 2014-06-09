c91fb9bd5d2851726c51c7a69b1d8df93cb85552
Alvin Vogelzang <alvinvogelzang@gmail.com>
2014-06-09 23:38:09 +0200

    Application controller next topic and progress dynamic

--- app/scripts/controllers/application_controller.js
+++ app/scripts/controllers/application_controller.js
@@ -1,4 +1,19 @@
+BecomeTjunaFish.LessonController = Ember.ObjectController.extend({
+  progressCalc: function(){
+    var topicsTotal = this.get('topics.length'),
+        topicsCompleted = this.get('topics').filterBy('isCompleted', true).get('length'),
+        assignmentsTotal = this.get('assignments.length'),
+        assignmentsCompleted = this.get('assignments').filterBy('isCompleted', true).get('length'),
+        progressResult =  (((topicsCompleted + assignmentsCompleted) / (topicsTotal + assignmentsTotal)) * 100),
+        progressResultRoundUp = Math.round(progressResult);
+        this.set('progress', progressResultRoundUp);
+    return progressResultRoundUp;
+  }
+  
+});
+
 BecomeTjunaFish.LessonTopicController = Ember.ObjectController.extend({
+  needs: ['lesson'], 
   isCompleted: function(key, value){
     var model = this.get('model');
 
@@ -9,15 +24,31 @@ BecomeTjunaFish.LessonTopicController = Ember.ObjectController.extend({
       // property being used as a setter
       model.set('isCompleted', value);
       model.save();
+      if (value === true){
+        $('.popUpReward').addClass('show');
+      }
       return value;
     }
   }.property('model.isCompleted'),
 
-  actions: {
-     nextPost: function(){
-      var topic = this.get('controllers');
-      console.log(topic);
-    }
-  }
-  
+  nextPost: function() {
+    this.advancePost(1);
+  },
+  nextTopic: function() {
+    var topics = this.get('controllers.lesson.topics'),
+        index = topics.indexOf(this.get('model')),
+        nextObject = topics.objectAt( index + 1 );
+        console.log(nextObject);
+        return nextObject;
+  }.property('id'),
+    
+     nextTopic: function(delta) {
+    var model = this.get('model'),
+        id = parseInt(model.get('id')),
+        nextObject = id + 1;
+        console.log(nextObject);
+        return nextObject;
+  }.property('id')
+    
+
 });
